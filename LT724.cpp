c9de723ae8636002cd3946c110ff0819a19c7145
imaniketrajput <as.thakuraniket@gmail.com>
2026-06-22 15:12:30 +0530

    a

---
+++ LT724.cpp
@@ -0,0 +1,55 @@
+/*Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+Example 3:
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0 */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1,7,3,6,5,6};
+    
+    int totalSum = 0;
+    for(int el : arr){
+        totalSum += el;
+    }
+
+    int leftSum = 0;
+
+    for(int i=0; i<arr.size(); i++){
+        int rightSum = totalSum - leftSum - arr[i];
+
+        if(leftSum == rightSum)
+        {
+            cout << i;
+            return 0;
+        }
+        
+        leftSum += arr[i];
+    }
+
+
+    
+
+    return 0;
+}
